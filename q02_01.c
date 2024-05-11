74a209833594b25a5a0607058fd88113702bfa8b
Prisca R. Manurung <friskarevalinamanurung@gmail.com>
2024-05-11 10:21:44 +0700

    test

--- q02_01.c
+++ q02_01.c
@@ -1,7 +1,9 @@
-// NIM - Name
-// NIM - Name
+// 12S23006 - Cardolan Sinaga
+// 12S23006 - Prisca Manurung 
 
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 #include "./libs/dorm.h"
 #include "./libs/student.h"
 
@@ -10,3 +12,4 @@ int main(int _argc, char **_argv)
     // codes
     return 0;
 }
+int x 
